70f21e84bb1d7e01f30225d502981edf2a400bfe
Arun S <arun3050@gmail.com>
2022-08-19 12:05:52 +0530

    Uploading Multiple files to AWS S3 v2 completed

--- backend/app.js
+++ backend/app.js
@@ -9,18 +9,22 @@ const app = express();
 // --------------
 // BASIC
 // --------------
+
+// ------------------------------------------------------------------------------
 // BASIC - Single File Upload
 const upload = multer({ dest: "uploads/" });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   res.json({ status: "success" });
 });
 
+// ------------------------------------------------------------------------------
 // BASIC - Multiple File Upload (here limited to 3)
 const uploads = multer({ dest: "uploads/" });
 app.post("/api/uploads", uploads.array("file", 3), (req, res) => {
   res.json({ status: "success" });
 });
 
+// ------------------------------------------------------------------------------
 // BASIC - Multiple Fild File Upload (here limited to 3)
 const uploadFile = multer({ dest: "uploads/" });
 const multiUpload = uploadFile.fields([
@@ -32,9 +36,9 @@ app.post("/api/multiupload", multiUpload, (req, res) => {
   res.json({ status: "success" });
 });
 
-// --------------
+// ------------------------------------------------------------------
 // INTERMEDIATE
-// --------------
+// ------------------------------------------------------------------
 // Custom Filename -  Multiple File Upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -64,9 +68,9 @@ app.post("/api/custom/upload", uploaduni.array("file", 3), (req, res) => {
   res.json({ status: "success" });
 });
 
-// --------------
+// ------------------------------------------------------------------
 // ERROR HANDLING
-// --------------
+// ------------------------------------------------------------------
 app.use((error, req, res, next) => {
   if (error instanceof multer.MulterError) {
     if (error.code === "LIMIT_FILE_SIZE") {
@@ -83,9 +87,12 @@ app.use((error, req, res, next) => {
   }
 });
 
-// --------------
-// S3 UPLOADS
-// --------------
+// ------------------------------------------------------------------
+// S3 UPLOADS - version 2
+// ------------------------------------------------------------------
+
+// ------------------------------------------------------------------------------
+// Upload Single file to S3 v2
 const temp_storage = multer.memoryStorage();
 // File Filter - Upload Image file only
 const fileFilterS3 = (req, file, cb) => {
@@ -107,5 +114,37 @@ app.post("/api/s3/upload", uploadS3.array("file", 3), async (req, res) => {
   const result = await s3Uploadv2(firstFile);
   res.json({ status: "success", result });
 });
+
+// ------------------------------------------------------------------------------
+// Upload Multiple file to S3 v2
+const temp_storage2 = multer.memoryStorage();
+const fileFilterS3_2 = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === "image") {
+    cb(null, true);
+  } else {
+    cb(new MulterError("LIMIT_UNEXPECTED_FILE"), false);
+  }
+};
+// File Size of 1Mb && Number of File Limit
+const uploadS3_2 = multer({
+  temp_storage,
+  fileFilterS3_2,
+  limits: { fileSize: 1000000, files: 2 },
+});
+app.post("/api/s3/uploads", uploadS3_2.array("file", 3), async (req, res) => {
+  try {
+    const results = await s3Uploadsv2(req.files);
+    console.log(results);
+    return res.json({ status: "success", results });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// ------------------------------------------------------------------
+// S3 UPLOADS - version 3
+// ------------------------------------------------------------------
+
+
 
 app.listen(4000, () => console.log("Listening on PORT 4000"));
--- backend/s3Service.js
+++ backend/s3Service.js
@@ -1,13 +1,30 @@
 const { S3 } = require("aws-sdk");
 const uuid = require("uuid").v4;
 
+// Upload Single file to S3
 exports.s3Uploadv2 = async (file) => {
   const s3 = new S3();
-
   const param = {
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: `uploads/${uuid()}-${file.originalname}`,
     Body: file.buffer,
   };
   return await s3.upload(param).promise();
+};
+
+// Upload Multiple file to S3
+exports.s3Uploadsv2 = async (files) => {
+  const s3 = new S3();
+
+  const params = files.map((file) => {
+    return {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `uploads/${uuid()}-${file.originalname}`,
+      Body: file.buffer,
+    };
+  });
+
+  return await Promise.all(
+    params.map((param) => s3.upload(param).promise())
+  );
 };
